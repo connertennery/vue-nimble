2c61030bc61bf20cba41500e2455c5528a5f3e72
Conner Tennery <connertennery@gmail.com>
2019-02-19 18:11:51 -0600

    Initial commit

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import NText from './views/NText.vue';
+
+Vue.use(Router);
+
+export default new Router({
+	routes: [
+		{
+			path: '/',
+			name: 'home',
+			component: Home,
+		},
+		{
+			path: '/NText',
+			name: 'ntext',
+			component: NText,
+		},
+		{
+			path: '/about',
+			name: 'about',
+			// route level code-splitting
+			// this generates a separate chunk (about.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+		},
+	],
+});
---
+++ src/views/NText.vue
@@ -0,0 +1,131 @@
+<template>
+	<div class="NText">
+		<div class="source">
+			<div class="sectionTitle">Source</div>
+			<textarea v-model="sourceText" @keyup="sourceText_keyUp"/>
+		</div>
+		<div class="pattern">
+			<div class="sectionTitle">Pattern</div>
+			<textarea v-model="patternText" @keyup="patternText_keyUp"/>
+			<div class="parsingConfig">
+				Column Separater
+				<input v-model="columnSeparater">
+				Row Separater
+				<input v-model="rowSeparater">
+			</div>
+		</div>
+		<div class="output">
+			<div class="sectionTitle">Output</div>
+			<textarea v-model="outputText"/>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "NText",
+	components: {},
+	props: {},
+	data() {
+		return {
+			sourceText: "",
+			patternText: "",
+			outputText: "",
+			columnSeparater: ",",
+			rowSeparater: "\\n"
+		};
+	},
+	computed: {
+		_rowSeparater: function() {
+			return this.replaceNormalWithLiterals(this.rowSeparater);
+		},
+		_columnSeparater: function() {
+			return this.replaceNormalWithLiterals(this.columnSeparater);
+		}
+	},
+	methods: {
+		replaceNormalWithLiterals(s) {
+			return s.replace("\\n", "\n").replace("\\t", "\t");
+		},
+		sourceText_keyUp() {
+			this.stampOutput();
+		},
+		patternText_keyUp() {
+			this.stampOutput();
+		},
+		stampOutput() {
+			this.outputText = "";
+			let rows = this.sourceText.split(this._rowSeparater);
+
+			var output = "";
+
+			rows.forEach(row => {
+				var s = row;
+				let currentLine = this.patternText;
+				let items = row.split(this._columnSeparater);
+
+				//Current problem is that $10 gets replaced by $1 and becomes $1 + '0'
+				//Could operate in reverse, going from highest index to lowest, but that may come with its own challenges.
+				//And if someone wants dollar signs in their source text, that will get overriden as well.
+				//Maybe find all of the indexes and cut them out then replace instead of a full replace all?
+				items.forEach((item, index) => {
+					currentLine = currentLine.replace(
+						new RegExp(
+							`\$${index + 1}`.replace(
+								/[.*+?^${}()|[\]\\]/g,
+								"\\$&"
+							),
+							"g"
+						),
+						item
+					);
+				});
+				output += currentLine + this._rowSeparater;
+			});
+
+			this.outputText = output;
+		}
+	}
+};
+</script>
+
+<style scoped>
+.NText {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+
+	background-color: rgb(160, 100, 220);
+	padding: 10px;
+	border-radius: 10px;
+}
+.NText textarea {
+	width: 98%;
+	min-height: 200px;
+	outline: none;
+	border-radius: 10px;
+	padding: 10px;
+
+	font-size: 12pt;
+}
+.source {
+}
+.sectionTitle {
+	font-size: 14pt;
+	color: white;
+	text-align: left;
+	margin-left: 12px;
+}
+.pattern {
+}
+.parsingConfig {
+	display: flex;
+	color: white;
+}
+.parsingConfig input {
+	margin-left: 8px;
+	margin-right: 12px;
+}
+.output {
+}
+</style>
